000835e46647eebe2062446938fdd6f7fdfb01e5
rosinabrooker <60344962+rosinabrooker@users.noreply.github.com>
2020-02-04 12:40:21 +0000

    Update functions.php

--- wp-content/themes/practice/functions.php
+++ wp-content/themes/practice/functions.php
@@ -6,7 +6,7 @@ function enqueue_parent_styles() {
 }
 
 function practice_files() {
-   wp_enqueue_style( 'main-style', get_stylesheet_uri(), filemtime(get_stylesheet_directory() . '/style.css'), 'all');
+   wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/style.css', array(), '3.0.0', "all");
 }
 
 add_action( 'wp_enqueue_scripts', 'practice_files');
